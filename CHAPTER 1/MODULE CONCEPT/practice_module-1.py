1a8525fb214212f23c64e3227d6ec94ca41aa4c1
gokulbagade <gokulbagade5@gmail.com>
2026-07-05 19:14:18 +0530

    practice_module updated

--- CHAPTER 1/MODULE CONCEPT/practice_module-1.py
+++ CHAPTER 1/MODULE CONCEPT/practice_module-1.py
@@ -1,7 +1,21 @@
 import calc
+print("enter the value of a , b :")
+num1=float(input("enter a value of 'a' :"))
+num2=float(input("enter a value of 'b' :"))
 
-try:
-    print(calc.add(4,5))
-except Exception as e:
-    print(e)
+input_method=input('''  1.add
+    2.sub
+    3.mult
+    4,div ''')
+
+if input_method == "1":
+    print(calc.add(num1,num2))
+elif input_method == "2":
+    print(calc.sub(num1,num2))
+elif input_method == "3":
+    print(calc.mult(num1,num2))
+elif input_method == "4":
+    print(calc.div(num1,num2))
+else:
+    print("Error: add a valid option")
 
